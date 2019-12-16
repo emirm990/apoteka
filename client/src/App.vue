ab98b7300521584361c5fecf4c04e0afb357d47e
Emir Mekić <emir.mekic@live.com>
2019-12-16 17:38:55 +0100

    dashboard route link added

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,20 +1,26 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <router-link to="/">Home</router-link>|
+      <router-link to="/about">About</router-link>|
+      <router-link to="/dashboard">Dashboard</router-link>
     </div>
     <router-view />
   </div>
 </template>
 
 <style lang="scss">
+* {
+  box-sizing: border-box;
+}
 #app {
   font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+  max-width: 1280px;
+  margin: 0 auto;
 }
 
 #nav {
