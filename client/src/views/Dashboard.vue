5b6449c6e31a5e5f8d03a40dae0fc305a2c694b8
Emir Mekić <emir.mekic@live.com>
2019-12-16 17:39:22 +0100

    Create Dashboard.vue

---
+++ client/src/views/Dashboard.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+    <Items :editable="true" />
+  </div>
+</template>
+<script>
+import Items from "@/components/Items.vue";
+export default {
+  name: "dashboard",
+  components: {
+    Items
+  }
+};
+</script>
