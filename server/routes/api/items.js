225ca50cb7a19d27adfaab0931453e19c9c3ef54
Emir Mekić <emir.mekic@live.com>
2019-12-16 17:40:34 +0100

    changed query so it select only needed fields

--- server/routes/api/items.js
+++ server/routes/api/items.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const connection = require("../../db/connection");
 
 router.get("/", async (req, res) => {
-  await connection.query("SELECT * FROM items", function(err, rows) {
+  await connection.query("SELECT i.id,name,description,stock,price,picture,extension,item_id FROM items i LEFT JOIN images p ON i.id=p.item_id", function (err, rows) {
     if (err) {
       res.status(503).send({ error: err });
     } else {
