a36cf11b20ff3fa324fd3165a29cefae2f9c4ab6
Emir Mekić <emir.mekic@live.com>
2019-12-17 20:23:04 +0100

    Create EditItems.vue

---
+++ client/src/views/EditItems.vue
@@ -0,0 +1,12 @@
+<template>
+  <Items :editable="true" />
+</template>
+<script>
+import Items from "@/components/Items.vue";
+export default {
+  name: "EditItems",
+  components: {
+    Items
+  }
+};
+</script>
