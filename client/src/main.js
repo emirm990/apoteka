c8d6cfd2d22a3b334d2ca61bd203162b9bed9b59
Emir Mekić <emir.mekic@live.com>
2019-12-17 20:22:39 +0100

    removed EventHub

--- client/src/main.js
+++ client/src/main.js
@@ -2,9 +2,7 @@ import Vue from "vue";
 import App from "./App.vue";
 import "./registerServiceWorker";
 import router from "./router";
-import EventHub from "vue-event-hub";
 
-Vue.use(EventHub);
 Vue.config.productionTip = false;
 new Vue({
   router,
