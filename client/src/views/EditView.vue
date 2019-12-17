f8bdd14adbd766a58745c947d32218dfc998585f
Emir Mekić <emir.mekic@live.com>
2019-12-17 17:38:39 +0100

    Create EditView.vue

---
+++ client/src/views/EditView.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="about">
+    <h1>This is an Edit View</h1>
+    <EditItem :id="id" />
+  </div>
+</template>
+<script>
+import EditItem from "@/components/EditItem.vue";
+
+export default {
+  name: "EditView",
+  components: {
+    EditItem
+  },
+  props: {
+    id: Number
+  }
+};
+</script>
