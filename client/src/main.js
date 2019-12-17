6d688d2239edda30e3c5ce65a218a5304da141bd
Emir Mekić <emir.mekic@live.com>
2019-12-17 17:38:30 +0100

    Update main.js

--- client/src/main.js
+++ client/src/main.js
@@ -2,9 +2,10 @@ import Vue from "vue";
 import App from "./App.vue";
 import "./registerServiceWorker";
 import router from "./router";
+import EventHub from "vue-event-hub";
 
+Vue.use(EventHub);
 Vue.config.productionTip = false;
-
 new Vue({
   router,
   render: h => h(App)
