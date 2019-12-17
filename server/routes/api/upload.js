7421fc5896482703e5ed2d9adde2beda3455dace
Emir Mekić <emir.mekic@live.com>
2019-12-17 17:37:53 +0100

    sync photo names in db and on the server

--- server/routes/api/upload.js
+++ server/routes/api/upload.js
@@ -7,6 +7,7 @@ const multer = require('multer');
 const destination = path.resolve(__dirname, '../../public/uploads/images');
 const upload = multer({ dest: destination });
 const connection = require("../../db/connection");
+const deleteFiles = require('../../delete');
 
 console.log(destination);
 router.post("/", upload.single('photo'), async (req, res) => {
@@ -14,13 +15,13 @@ router.post("/", upload.single('photo'), async (req, res) => {
         let extArray = req.file.mimetype.split("/");
         let extension = extArray[extArray.length - 1];
         let filename = path.join(destination, req.file.filename) + "." + extension;
-        await connection.query(`INSERT INTO images (picture,extension,item_id) VALUES (${JSON.stringify(
-            req.file.filename
-        )}, ${JSON.stringify(extension)}, ${req.query.id})`, function (err) {
+
+        await connection.query(`INSERT INTO images(picture, extension, item_id) VALUES (${JSON.stringify(req.file.filename)}, ${JSON.stringify(extension)}, ${req.query.id}) ON DUPLICATE KEY UPDATE picture=${JSON.stringify(req.file.filename)},extension=${JSON.stringify(extension)}`, function (err) {
             if (err) {
                 res.status(503).send({ error: err })
             } else {
-                res.status(201).send({ Message: "Image uploaded!" })
+                deleteFiles.deleteFiles();
+                res.status(201).send({ message: "Picture uploaded!" })
             }
         })
         //res.json(req.file);
@@ -30,4 +31,13 @@ router.post("/", upload.single('photo'), async (req, res) => {
     }
 });
 
+router.get("/", async (req, res) => {
+    await connection.query(`SELECT * FROM images WHERE item_id=${req.query.id}`, function (err, rows) {
+        if (err) {
+            res.status(503).send({ error: err });
+        } else {
+            res.status(200).send({ item: rows });
+        }
+    });
+})
 module.exports = router;
