6ec7afa213f91d7868dcffb896323aea079f774b
Emir Mekić <emir.mekic@live.com>
2019-12-17 17:38:37 +0100

    Update Dashboard.vue

--- client/src/views/Dashboard.vue
+++ client/src/views/Dashboard.vue
@@ -1,15 +1,18 @@
-<template>
+<template >
   <div class="about">
     <h1>This is an about page</h1>
     <Items :editable="true" />
+    <NewItem />
   </div>
 </template>
 <script>
 import Items from "@/components/Items.vue";
+import NewItem from "@/components/NewItem.vue";
 export default {
   name: "dashboard",
   components: {
-    Items
+    Items,
+    NewItem
   }
 };
 </script>
