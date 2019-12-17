09aa4e29ac619beafff0741ab89a65f1da220dfb
Emir Mekić <emir.mekic@live.com>
2019-12-17 20:22:28 +0100

    Update NewItem.vue

--- client/src/components/NewItem.vue
+++ client/src/components/NewItem.vue
@@ -1,34 +1,38 @@
 <template>
-  <div>
-    <form v-on:submit.prevent="addNewItem">
-      <div class="form-group">
-        <label for="name">Name</label>
-        <input type="text" name="name" id="name" v-model="name" />
-      </div>
-      <div class="form-group">
-        <label for="description">Description</label>
-        <textarea
-          type="text"
-          name="description"
-          id="description"
-          rows="4"
-          cols="50"
-          v-model="description"
-        />
-      </div>
-      <div class="form-group">
-        <label for="price">Price</label>
-        <input type="number" step="0.05" name="number" id="number" v-model="price" />
-      </div>
-      <div class="form-group">
-        <label for="stock">Stock</label>
-        <input type="number" name="stock" id="stock" v-model="stock" />
-      </div>
-      <div class="form-group">
-        <input type="submit" value="Create" />
-      </div>
-    </form>
-  </div>
+  <form v-on:submit.prevent="addNewItem">
+    <div class="form-group">
+      <label for="name">Name</label>
+      <input type="text" name="name" id="name" v-model="name" />
+    </div>
+    <div class="form-group">
+      <label for="description">Description</label>
+      <textarea
+        type="text"
+        name="description"
+        id="description"
+        rows="4"
+        cols="50"
+        v-model="description"
+      />
+    </div>
+    <div class="form-group">
+      <label for="price">Price</label>
+      <input
+        type="number"
+        step="0.05"
+        name="number"
+        id="number"
+        v-model="price"
+      />
+    </div>
+    <div class="form-group">
+      <label for="stock">Stock</label>
+      <input type="number" name="stock" id="stock" v-model="stock" />
+    </div>
+    <div class="form-group">
+      <input type="submit" value="Create" />
+    </div>
+  </form>
 </template>
 <script>
 import NewItemService from "../../NewItemService";
