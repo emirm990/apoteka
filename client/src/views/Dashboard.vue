c243e34b76c2a22e496c4107a29b2f7134be74d4
Emir Mekić <emir.mekic@live.com>
2019-12-18 15:36:50 +0100

    Update Dashboard.vue

--- client/src/views/Dashboard.vue
+++ client/src/views/Dashboard.vue
@@ -1,12 +1,83 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
-    <router-link to="/dashboard/edit">Edit Items</router-link>
-    <router-link to="/dashboard/new">New Item</router-link>
+    <form @submit.prevent="login" v-if="!auth">
+      <div class="form-group">
+        <label for="name">email</label>
+        <input type="text" name="name" id="name" v-model="name" autocomplete="username" />
+      </div>
+      <div class="form-group">
+        <label for="password">password</label>
+        <input
+          type="password"
+          name="password"
+          id="password"
+          v-model="password"
+          autocomplete="current-password"
+        />
+      </div>
+      <div class="form-group">
+        <input type="submit" name="submit" id="submit" value="login" />
+      </div>
+    </form>
+    <div v-if="auth">
+      <button @click="logout">Logout</button>
+    </div>
+    <div v-if="auth">
+      <button>
+        <router-link :to="{ name: 'edit', params: {auth: this.auth}}">Edit Items</router-link>
+      </button>
+      <button>
+        <router-link :to="{ name: 'new', params: {auth: this.auth}}">New Item</router-link>
+      </button>
+    </div>
   </div>
 </template>
 <script>
+import AuthService from "../../AuthService";
+import SessionService from "../../SessionService";
 export default {
-  name: "dashboard"
+  name: "dashboard",
+  data() {
+    return {
+      name: "",
+      password: "",
+      auth: false
+    };
+  },
+  created() {
+    this.checkSession();
+  },
+  methods: {
+    async login() {
+      let response = await AuthService.login(this.name, this.password);
+      if (response.data == "Authenticated") {
+        this.auth = true;
+      }
+    },
+    async logout() {
+      let response = await AuthService.logout();
+      // eslint-disable-next-line no-console
+      console.log(response);
+      if (response.data == "Logout succesful") {
+        this.auth = false;
+      }
+    },
+    async checkSession() {
+      let response = await SessionService.sessionActive();
+      if (response.data == "Authenticated") {
+        this.auth = true;
+      }
+    }
+  }
 };
 </script>
+<style lang="scss" scoped>
+button {
+  margin: 0 10px;
+  a {
+    padding: 10px 20px;
+    display: inline-block;
+  }
+}
+</style>
