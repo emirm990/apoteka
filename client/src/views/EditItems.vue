dbe09783ead2a06b8804b0673d5f59edfec2749c
Emir Mekić <emir.mekic@live.com>
2019-12-18 15:36:32 +0100

    Update EditItems.vue

--- client/src/views/EditItems.vue
+++ client/src/views/EditItems.vue
@@ -1,10 +1,18 @@
 <template>
-  <Items :editable="true" />
+  <div>
+    <Items v-if="auth" :editable="true" :auth="this.auth" />
+    <div v-else>
+      <p>You don't have an acces to this page!</p>
+    </div>
+  </div>
 </template>
 <script>
 import Items from "@/components/Items.vue";
 export default {
   name: "EditItems",
+  props: {
+    auth: Boolean
+  },
   components: {
     Items
   }
